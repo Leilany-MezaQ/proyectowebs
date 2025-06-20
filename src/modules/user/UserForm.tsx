f55ff17a4b45729600b15548004668e857eea464
Lei💻 <leilany_3041230345@utd.edu.mx>
2025-06-20 12:17:50 -0600

    Add files via upload

---
+++ src/modules/user/UserForm.tsx
@@ -0,0 +1,13 @@
+import { Form } from 'antd'
+import React from 'react'
+
+function UserForm() {
+    const [form]=Form.useForm()
+  return (
+    <div>
+      UserForm
+    </div>
+  )
+}
+
+export default UserForm
